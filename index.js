cf4683c23f2c23b67641feec97d7d425ea2e1bf8
Sebastian Perez <103871085+sebadev-img@users.noreply.github.com>
2023-12-15 18:58:57 -0300

    Register: Pantalla de Register (#3)
    * Login: se creo la pantalla de login
    
    * Register: se agrego la pantalla de register

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/login/login.html");
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/public/register/register.html");
 });
 
 app.post("/loginUser", async (req, res) => {
@@ -30,6 +33,50 @@ app.post("/loginUser", async (req, res) => {
   }
 });
 
+
+app.post("/registerUser", async (req, res) => {
+  try {
+    const data = req.body;
+
+    const url = API_URL + "/auth/register";
+    const registerResponse = await postData(url, {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      password: data.password
+    });
+    console.log(registerResponse);
+
+    const loginURL = API_URL + "/auth/login";
+    try {
+      const loginResponse = await postData(loginURL, {
+        email: data.email,
+        password: data.password
+      });
+      console.log(loginResponse);
+
+      if (loginResponse.token) {
+        const token = loginResponse.token;
+        const arsURL = API_URL + "/accounts?currency=ARS";
+        const usdURL = API_URL + "/accounts?currency=USD";
+        try {
+          const accountArs = await postData(arsURL, null, token);
+          console.log(accountArs);
+          const accountUsd = await postData(usdURL, null, token);
+          console.log(accountUsd);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      res.json(loginResponse);
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
---
+++ public/register/register.js
@@ -0,0 +1,35 @@
+const inputFirstName = document.getElementById("InputFirstName");
+const inputLastName = document.getElementById("InputLastName");
+const inputEmail = document.getElementById("InputEmail");
+const inputPassword = document.getElementById("InputPassword");
+const btnRegister = document.getElementById("btnRegister");
+
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+btnRegister.addEventListener("click", async (e) => {
+  const firstName = inputFirstName.value;
+  const lastName = inputLastName.value;
+  const email = inputEmail.value;
+  const password = inputPassword.value;
+  if (firstName && lastName && emailRegExp.test(email) && password) {
+    e.preventDefault();
+    console.log("request: ", firstName, lastName, email, password);
+    try {
+      const response = await axios.post("/registerUser", {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+      });
+      console.log("response: ", response.data);
+      if (response.data.token) {
+        sessionStorage.setItem("token", "Bearer " + response.data.token);
+        console.log(sessionStorage.getItem("token"));
+        window.open("http://localhost:3000/home", "_self");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
