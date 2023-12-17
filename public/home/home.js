b41abc1c65c8125b9dfac8bfd04ed464f9377c01
sebastian <sebastiandev.img@gmail.com>
2023-12-17 14:27:45 -0300

    Logout: se creo la funcionalidad de cerrar sesion

--- public/home/home.js
+++ public/home/home.js
@@ -4,6 +4,12 @@ const accountUsdCVU = document.getElementById("accountUsd-CVU");
 const accountUsdBalance = document.getElementById("accountUsd-Balance");
 const transactionList = document.getElementById("transaction-list");
 const btnMas = document.getElementById("btn-mas");
+const btnLogout = document.getElementById("btn-logout");
+
+btnLogout.addEventListener("click", () => {
+  sessionStorage.clear();
+  window.open("http://localhost:3000", "_self");
+});
 
 btnMas.addEventListener("click", () => {
   window.open("http://localhost:3000/transactions", "_self");
