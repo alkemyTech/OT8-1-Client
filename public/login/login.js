6b27ee896fe63865317b10a199ac5d06391c53f8
sebastian <sebastiandev.img@gmail.com>
2023-12-16 23:24:09 -0300

    Login:se actualizo la pantalla de login

--- public/login/login.js
+++ public/login/login.js
@@ -6,20 +6,24 @@ const emailRegExp =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 btnLogin.addEventListener("click", async (e) => {
+  console.log("clic");
   const userEmail = inputEmail.value;
   const userPassword = inputPassword.value;
   if (emailRegExp.test(userEmail) && userPassword) {
     e.preventDefault();
-    console.log("request: ", userEmail, userPassword);
+    console.log(userEmail, userPassword);
     try {
       const response = await axios.post("/loginUser", {
         userEmail: userEmail,
         userPassword: userPassword
       });
-      console.log("response: ", response.data);
-      if (response.data.token) {
-        sessionStorage.setItem("token", "Bearer " + response.data.token);
-        console.log(sessionStorage.getItem("token"));
+      console.log(response.data);
+      if (response.data.jwt) {
+        sessionStorage.setItem("token", response.data.jwt);
+        sessionStorage.setItem("userId", response.data.userId);
+        sessionStorage.setItem("firstName", response.data.firstName);
+        sessionStorage.setItem("lastName", response.data.lastName);
+        sessionStorage.setItem("email", response.data.userEmail);
         window.open("http://localhost:3000/home", "_self");
       }
     } catch (error) {
