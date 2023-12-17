62cdfec4eb725dc333e1515e1f785c93be311da5
Sebastian Perez <103871085+sebadev-img@users.noreply.github.com>
2023-12-17 14:31:43 -0300

    Feature/register (#6)
    * T8S2-139:se arreglaron errores en la api service
    
    * Login:se actualizo la pantalla de login
    
    * Register:se actualizo la pantalla de register

--- public/register/register.js
+++ public/register/register.js
@@ -1,5 +1,7 @@
 const inputFirstName = document.getElementById("InputFirstName");
 const inputLastName = document.getElementById("InputLastName");
+const inputFirstName = document.getElementById("InputFirstName");
+const inputLastName = document.getElementById("InputLastName");
 const inputEmail = document.getElementById("InputEmail");
 const inputPassword = document.getElementById("InputPassword");
 const btnRegister = document.getElementById("btnRegister");
@@ -8,13 +10,14 @@ const emailRegExp =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 btnRegister.addEventListener("click", async (e) => {
+  console.log("clic");
   const firstName = inputFirstName.value;
   const lastName = inputLastName.value;
   const email = inputEmail.value;
   const password = inputPassword.value;
   if (firstName && lastName && emailRegExp.test(email) && password) {
     e.preventDefault();
-    console.log("request: ", firstName, lastName, email, password);
+    console.log(firstName, lastName, email, password);
     try {
       const response = await axios.post("/registerUser", {
         firstName: firstName,
@@ -22,9 +25,13 @@ btnRegister.addEventListener("click", async (e) => {
         email: email,
         password: password
       });
-      console.log("response: ", response.data);
-      if (response.data.token) {
-        sessionStorage.setItem("token", "Bearer " + response.data.token);
+      console.log(response.data);
+      if (response.data.jwt) {
+        sessionStorage.setItem("token", response.data.jwt);
+        sessionStorage.setItem("userId", response.data.userId);
+        sessionStorage.setItem("firstName", response.data.firstName);
+        sessionStorage.setItem("lastName", response.data.lastName);
+        sessionStorage.setItem("email", response.data.userEmail);
         console.log(sessionStorage.getItem("token"));
         window.open("http://localhost:3000/home", "_self");
       }
